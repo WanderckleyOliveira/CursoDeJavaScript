4fb9a871b52f1184dda9d73e8ac34c5c64e9bbb3
Wanderckley Oliveira <Keiu_19@hotmail.com>
2023-11-09 14:31:56 -0400

    Aprender o console.log e Variaveis

--- aula.js
+++ aula.js
@@ -1,2 +1,16 @@
+// - Console.log serve para mostrar algo no console do navegar.
+
 console.log("Wanderckley");
 console.log("Bem vindo ao curso");
+
+
+/* let é forma atual de declarar variaveis
+    var é a forma antiga de declarar variaveis
+    - variaveis serve para armazenar dados*/
+
+let idade = 5;
+console.log(idade);
+let altura = 1.79;
+console.log(altura);
+
+
