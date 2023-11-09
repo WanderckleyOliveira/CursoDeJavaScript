5f53f6aa9cd9482afef534e26b6f6be207090297
Wanderckley Oliveira <Keiu_19@hotmail.com>
2023-11-09 14:49:57 -0400

    variaveis let , const e suas atribuições

--- aula.js
+++ aula.js
@@ -1,16 +1,32 @@
 // - Console.log serve para mostrar algo no console do navegar.
 
+/*
 console.log("Wanderckley");
 console.log("Bem vindo ao curso");
-
+*/
 
 /* let é forma atual de declarar variaveis
     var é a forma antiga de declarar variaveis
     - variaveis serve para armazenar dados*/
 
+/*
 let idade = 5;
 console.log(idade);
 let altura = 1.79;
+console.log(altura);*/
+
+// Exercicio imprima na tela tres variaveis diferente
+
+let nome = 'Wanderckley';
+let peso = 104;
+let altura = 1.79;
+const cor = 'preto';
+
+peso = 200;
+
+console.log(nome);
+console.log(peso);
 console.log(altura);
+console.log(cor);
 
 
